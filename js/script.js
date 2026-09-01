3afc2db9551790bb7c53ec25053314efefc10325
Dapjzl <emmanueladedapo2019@gmail.com>
2026-09-01 10:05:57 +0100

    feat: update job titles and company names in index.html; enhance responsive design in CSS files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 (function ($) {
    "use strict";
     const $window = $(window);
-    const CONFIG = { mobileBreakpoint: 768, formAction: ["form-process.php", "review-form.php"] };
+    const CONFIG = { mobileBreakpoint: 991, formAction: ["form-process.php", "review-form.php"] };
 
     // Preloader
     $window.on("load", () => setTimeout(() => $(".sis-preloader").fadeOut(1000), 700));
